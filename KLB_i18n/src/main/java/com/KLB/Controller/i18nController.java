d8405cf7b5a3ce14bfee318aefe31317988099f0
Nguyễn Quốc Duy <127750881+Duy220807@users.noreply.github.com>
2024-08-05 10:04:53 +0700

    Fix

---
+++ KLB_i18n/src/main/java/com/KLB/Controller/i18nController.java
@@ -0,0 +1,29 @@
+package com.KLB.Controller;
+
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class i18nController {
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @GetMapping("/greet")
+    public String greet(@RequestParam(name = "lang", required = false) Locale locale) {
+        if (locale == null) {
+            locale = Locale.getDefault(); // Sử dụng ngôn ngữ mặc định nếu không có tham số lang
+        }
+        return messageSource.getMessage("greeting", null, locale);
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "Test successful!";
+    }
+}
